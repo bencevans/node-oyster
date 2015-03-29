8f77f5f6d83c270eaa061254aa5a0ffaacec01d1
Damien Laureaux <damien.laureaux@tuitravel.com>
2015-03-29 21:22:53 +0100

    Format test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var Oyster = require('./');
 
 var cards = [
-	"012345678910",
-	"123456789101",
-	"234567891012"
+  "012345678910",
+  "123456789101",
+  "234567891012"
 ];
 
 var oyster = Oyster(process.env.USER, process.env.PASS, function(err) {
@@ -12,12 +12,9 @@ var oyster = Oyster(process.env.USER, process.env.PASS, function(err) {
   console.log('Logged In');
   
   for(var i; i <= cards.length; i++) {
-	  oyster.balance(cards[i], function(err, balance) {
-	    
-	    if(err) throw err;
-	    
-	    console.log('Balance: £' + balance);
-	  });
-   }
-
+	oyster.balance(cards[i], function(err, balance) {    
+	  if(err) throw err;  
+	  console.log('Balance: £' + balance);
+	});
+  }
 });
