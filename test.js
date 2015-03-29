daf2b5698b91652591db2e0a1837496a62f39593
Damien Laureaux <damien.laureaux@tuitravel.com>
2015-03-29 18:42:35 +0100

    Update test.js with example of get multiple Oyster cards balance + add contributor infos

--- test.js
+++ test.js
@@ -1,10 +1,23 @@
 var Oyster = require('./');
 
+var cards = [
+	"012345678910",
+	"123456789101",
+	"234567891012"
+];
+
 var oyster = Oyster(process.env.USER, process.env.PASS, function(err) {
   if(err) throw err;
+  
   console.log('Logged In');
-  oyster.balance(function(err, balance) {
-    if(err) throw err;
-    console.log('Balance: £' + balance);
-  });
+  
+  for(var i; i <= cards.length; i++) {
+	  oyster.balance(cards[i], function(err, balance) {
+	    
+	    if(err) throw err;
+	    
+	    console.log('Balance: £' + balance);
+	  });
+   }
+
 });
